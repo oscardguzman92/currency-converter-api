ee97ca9102b7949557d51785d14247a4225a98e9
Oscar Daniel <oscardguzman92@gmail.com>
2025-05-31 10:38:23 -0500

    Initial project setup with Spring Boot

---
+++ src/main/java/com/mycompany/currency/converter/CurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.currency.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/currency/converter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.currency.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
